3cd27d328a423975e1ff025c4cfb486f0a8613e9
meer3kat <peiliguo@gmail.com>
2018-09-16 13:51:17 +0200

    add function to call for euler implicit with gamma variable

---
+++ fdimplicit.m
@@ -0,0 +1,44 @@
+function [s,vj] = fdimplicit(K,r,sigma,T,g,tn,sn)
+
+%% function to compute the call option price use implicit euler 
+% parameter
+% 
+% K = 15; strike price 
+% r = 0.1; risk free interest rate
+% sigma = 0.25; volatility
+% T = 0.5; option time 
+% g = gamma, and set gamma = 1 to inspect ;
+% tn: number of time points on discretization, and dt = (T - T0)/(tn-1)
+% sn: number of price points on discretizatin, and ds = (S - S0)/(sn-1)
+
+Smax = 4 * K; %the price range to compute
+t = linspace(0,T,tn); % time vector
+s = linspace(0,Smax,sn); % price vector
+dt = t(2)-t(1); 
+ds = s(2)-s(1); 
+vj = max(s-K,0); % boundary condition, at time = T, we know the value of option
+vj = vj(:); %force a column vector
+sig2 = sigma*sigma;
+%construct the tridiagonal matrix
+a = zeros(1,sn);
+b = zeros(1,sn);
+c = zeros(1,sn);
+for i = 1:1:sn %iterate at each grid point
+    a(i) = 0.5*dt*(r*i - sig2*i^(2*g)*ds^(2*g-2));
+    b(i) = 1 + dt*(sig2*i^(2*g)*ds^(2*g-2) + r);
+    c(i) = -0.5*dt*(r*i + sig2*i^(2*g)*ds^(2*g-2));
+
+end
+C = diag(a(3:end-1),-1) + diag(b(2:end-1)) + diag(c(2:end-2),1);
+ 
+for n = tn:-1:2
+    v = zeros(sn,1);
+    %v(1) = 0; already initialized 
+    v(sn) = Smax-K*exp(-r*(T-t(n-1)));
+    
+    f = vj(2:end-1); % take vj(2:end-1) exclude the boundary points 
+    f(end) = f(end) - c(end-1)*vj(end); % for the v(t(i-1), s(n-1))
+    v(2:end-1) = C\f;
+    vj = v;
+end
+end
