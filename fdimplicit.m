ac71a2c3650ae1f4c5d3052105d094d1dab9896e
meer3kat <peiliguo@gmail.com>
2018-09-16 17:00:54 +0200

    implicit function working now.

--- fdimplicit.m
+++ fdimplicit.m
@@ -2,7 +2,6 @@ function [s,vj] = fdimplicit(K,r,sigma,T,g,tn,sn)
 
 %% function to compute the call option price use implicit euler 
 % parameter
-% 
 % K = 15; strike price 
 % r = 0.1; risk free interest rate
 % sigma = 0.25; volatility
@@ -24,21 +23,22 @@ a = zeros(1,sn);
 b = zeros(1,sn);
 c = zeros(1,sn);
 for i = 1:1:sn %iterate at each grid point
-    a(i) = 0.5*dt*(r*i - sig2*i^(2*g)*ds^(2*g-2));
-    b(i) = 1 + dt*(sig2*i^(2*g)*ds^(2*g-2) + r);
-    c(i) = -0.5*dt*(r*i + sig2*i^(2*g)*ds^(2*g-2));
-
+    a2(i) = 0.5*dt*(r*i - sig2*i^(2*g)*ds^(2*g-2));
+    b2(i) = 1 + dt*(sig2*i^(2*g)*ds^(2*g-2) + r);
+    c2(i) = -0.5*dt*(r*i + sig2*i^(2*g)*ds^(2*g-2));
+    a(i) = 0.5*dt*(r*s(i)/ds - sig2*(s(i)^(2*g))/(ds*ds));
+    b(i) = 1 + dt*(sig2*(s(i)^(2*g)/(ds*ds)) + r);
+    c(i) = -0.5*dt*(r*s(i)/ds + sig2*(s(i)^(2*g)/(ds*ds)));
 end
-C = diag(a(3:end-1),-1) + diag(b(2:end-1)) + diag(c(2:end-2),1);
+D = diag(a(3:end-1),-1) + diag(b(2:end-1)) + diag(c(2:end-2),1);
  
 for n = tn:-1:2
     v = zeros(sn,1);
     %v(1) = 0; already initialized 
-    v(sn) = Smax-K*exp(-r*(T-t(n-1)));
-    
+    v(sn,1) = Smax-K*exp(-r*(T-t(n-1)));    
     f = vj(2:end-1); % take vj(2:end-1) exclude the boundary points 
-    f(end) = f(end) - c(end-1)*vj(end); % for the v(t(i-1), s(n-1))
-    v(2:end-1) = C\f;
+    f(end) = f(end) - c(end-1)*v(end); % for the v(t(i-1), s(n-1))
+    v(2:end-1) = D\f;
     vj = v;
 end
 end
